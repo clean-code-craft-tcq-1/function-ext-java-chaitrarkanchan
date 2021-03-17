5ae16740c95dff03b07761f67435be31380e57ef
chaitrarkanchan <49150199+chaitrarkanchan@users.noreply.github.com>
2021-03-17 12:57:53 +0530

    Update BatteryFactorAccumulator.java

--- vitals/BatteryFactorAccumulator.java
+++ vitals/BatteryFactorAccumulator.java
@@ -73,7 +73,7 @@ public class BatteryFactorAccumulator {
 			displayMaximumAlert(parameter);
 			return false;
 		}
-		if(parammeasure==true && status=="Low" ){
+		else if(parammeasure==true && status=="Low" ){
 		VitalFactorExceedMinLimit.put(parameter,val);
 			displayMinimumAlert(parameter);
 			return false;
